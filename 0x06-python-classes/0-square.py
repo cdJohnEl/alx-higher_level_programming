116dc19e60f6114bf08450598f521ab61f43e7ef
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-06-27 10:00:31 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """defines class square """
-class Square:
-    def __init__ (self, length):
-        self.length = length
 
-    def getArea (self):
-        return self.length ** 2
+class Square:
+    pass
