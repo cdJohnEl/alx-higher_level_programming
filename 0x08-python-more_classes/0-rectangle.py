021b7daf066a62216a5be251d414cf580bf646f5
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-07-03 09:16:05 -0700

    I added some files

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,8 @@
-#usr/bin/python3
-"""Defines an empty rectangle"""
+#!/usr/bin/python3
+"""
+Defines an empty class Rectangle
+"""
+
 
 class Rectangle:
     """Empty representation of a rectangle"""
