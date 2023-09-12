f8f731b7a0806a08e46cc9ac9816f0f5bb82592c
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-09-12 07:36:05 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < list.length; i++) {
-  console.log(list[i]);
+for (const list of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(list);
 }
