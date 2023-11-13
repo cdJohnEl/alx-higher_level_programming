c61fcb566b95c0803f4a3907ebcbc53b2f93f902
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-11-13 11:54:58 +0100

    Tasks 0 to 10 done

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+const number = parseInt(firstArg);
+
+if (!isNaN(number)) {
+    for (let i = 0; i < number; i++) {
+        console.log('C is fun');
+    }
+} else {
+    console.log('Missing number of occurrences');
+}
