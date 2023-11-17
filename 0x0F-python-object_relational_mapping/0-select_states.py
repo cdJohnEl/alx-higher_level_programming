efba18a0469fc0543b7c3f2492fcfb6478fb95d3
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-11-17 11:14:32 +0100

    Corrected errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,9 @@ This script lists all states from the database hbtn_0e_0_usa
 """
 
 if __name__ == '__main__':
+    """
+    Access the database and get the states from the database
+    """
     
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,10 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
     
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     
