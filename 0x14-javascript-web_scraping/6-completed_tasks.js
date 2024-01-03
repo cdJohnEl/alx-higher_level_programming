cbe012800931967137fb54437f9c4fafbb55a3c7
cdJohnEl <easykelchimdikejohn@gmail.com>
2024-01-03 16:01:32 +0100

    Done with all tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const completedTasks = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!completedTasks[todo.userId]) {
+        completedTasks[todo.userId] = 1;
+      } else {
+        completedTasks[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(completedTasks);
+});
